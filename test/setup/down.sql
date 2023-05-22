ba74fb207cfc6e078740fb197d5f16e2d17c9636
Jean Carlo Giambastiani Lopes <jean.lopes@hotmail.com.br>
2023-05-21 22:01:53 -0300

    remove unneeded drop comands

--- test/setup/down.sql
+++ test/setup/down.sql
@@ -1,12 +1,5 @@
 \set QUIET 'on'
 drop schema if exists bitemporal cascade;
--- drop procedure if exists remove_cascade_sp(text, sp.valid_period%type) cascade;
--- drop procedure if exists remove_cascade_s(text, s.valid_period%type) cascade;
--- drop procedure if exists remove_sp(text, sp.valid_period%type) cascade;
--- drop procedure if exists remove_s(text, s.valid_period%type) cascade;
--- drop procedure if exists save_sp(sp.s_id%type, sp.id%type, sp.state%type, sp.valid_period%type) cascade;
--- drop procedure if exists save_s(s.id%type, s.value%type, s.valid_period%type) cascade;
--- drop function if exists check_fk_s_sp cascade;
 drop table if exists sp cascade;
 drop table if exists s cascade;
 drop extension if exists btree_gist cascade;
